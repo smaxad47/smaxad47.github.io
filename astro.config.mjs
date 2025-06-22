a047d1a55c84ae9381df44ca5dcd3b02b2be75e3
smaxad47 <assassin.of.abyss@gmail.com>
2025-06-22 10:48:18 +0800

    ad2

--- astro.config.mjs
+++ astro.config.mjs
@@ -21,7 +21,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://blog.epyso.org/",
+  site: "https://epyso.org/",
   base: "/",
   trailingSlash: "always",
   integrations: [
